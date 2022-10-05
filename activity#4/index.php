800cff4ab5e1a7d413ca5abd242c958ee4c5aea5
joushsh <114554537+joushsh@users.noreply.github.com>
2022-10-05 13:05:15 +0800

    Add files via upload

---
+++ activity#4/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Joshua Castillo";
+    $description = "a student";
+    $img_url = "";
+
+    echo "My name is " . $name . "<br>";
+    echo "I'm " . $description . "<br>";
+    echo "Picture: ". $img_url . "<img style=width:50px;height:50px; src=profile.jpg";
+
+    ?>
+    
+</body>
+</html>
